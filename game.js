5a365843766c3bcbe4843de1ac0d5541e42f237f
Vincent <Vincent.ahlin99@gmail.com>
2023-11-02 17:37:23 +0100

    Adding other assets

--- game.js
+++ game.js
@@ -1,6 +1,6 @@
 const allDoors = document.getElementById("allDoors"); //Background look nice
 const backArrow = document.getElementById("backArrow"); //Click click click this
-const blueDoor = document.getElementById("blueDoor"); //Click
+const blueDoor = document.getElementsByClassName("blueDoor"); //Click
 const blueRoom = document.getElementById("blueRoom"); //Background
 const redRoom = document.getElementById("redRoom"); //Background
 const redDoor = document.getElementById("redDoor"); //Click
@@ -14,20 +14,8 @@ function startGame() {
   updateText(currentId);
 }
 
-//We don't want these first
-blueRoom.style.display = "none";
-redRoom.style.display = "none";
-
-//Now we click the blue door cuz fun and we do lil hidning
-//Also we do a lil updating of the currentID to display new text
-blueDoor.addEventListener("click", function () {
-  allDoors.style.display = "none";
-  blueRoom.style.display = "block";
-  updateText(2);
-});
-
 //This is takes boring static display text (gross)
-// Then update it with the current id! (COOL!)
+//Then update it with the current id! (COOL!)
 function updateText(id) {
   const displayText = document.getElementById("displayText");
   const textData = optionTexts.find((item) => item.id === id);
@@ -36,33 +24,6 @@ function updateText(id) {
     displayText.textContent = textData.text;
   }
 }
-
-//This just be like GO BACK! We don't like that room
-backArrow.addEventListener("click", function () {
-  allDoors.style.display = "block";
-  blueRoom.style.display = "none";
-  if (currentId > 1) {
-    currentId--;
-  }
-  updateText(currentId);
-});
-
-//Do we like this room?
-redDoor.addEventListener("click", function () {
-  redRoom.style.display = "block";
-  allDoors.style.display = "none";
-  updateText(3);
-});
-
-//NO TAKE ME BACK
-redRoomArrow.addEventListener("click", function () {
-  redRoom.style.display = "none";
-  allDoors.style.display = "block";
-  if (currentId > 1) {
-    currentId--;
-  }
-  updateText(currentId);
-});
 
 //WOW this be long :O
 const optionTexts = [
@@ -96,14 +57,3 @@ const optionTexts = [
 ];
 
 startGame();
-
-const clockNow = document.getElementById("timeRightNow");
-
-setInterval(printDate, 1000);
-
-function printDate() {
-  const date = new Date();
-  clockNow.textContent = date.toLocaleTimeString();
-}
-
-printDate();
