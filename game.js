272e824a9e0798c295f6c7b191821cbd321b1239
Vincent <Vincent.ahlin99@gmail.com>
2023-11-03 14:08:12 +0100

    Adding the visual scenes

--- game.js
+++ game.js
@@ -1,3 +1,10 @@
+const lockedDoorText = document.getElementById("lockedTitle");
+const inventory = document.getElementById("inventory");
+const blueKey = document.getElementById("BlueKey");
+const redKey = document.getElementById("redKey");
+const greenKey = document.getElementById("greenKey");
+const lockPick = document.getElementById("lockPick");
+const noteSheet = document.getElementById("noteSheet");
 const startRoomBg = document.getElementById("room"); //SCENE
 const brownRoomBg = document.getElementById("brownRoom"); //SCENE
 const backArrow = document.getElementById("backArrow"); // Click
@@ -10,12 +17,19 @@ const brownDoor = document.getElementById("brownDoor"); // Click
 const greenDoorBg = document.getElementById("greenDoorOpen"); //BACKGROUND
 const greenDoor = document.getElementById("greenDoor"); // Click
 const bookshelf = document.getElementById("bookshelf"); //Click
+const exploreBookshelf = document.getElementById("exploreBookshelf");
 const desk = document.getElementById("desk"); //Click
+const deskOpen = document.getElementById("deskOpen");
+const explorePiano = document.getElementById("pianoNo");
 const piano = document.getElementById("piano"); //Click
 
+lockedDoorText.style.opacity = 0;
 brownRoomBg.style.opacity = 0;
+exploreBookshelf.style.opacity = 0;
 bookshelf.style.opacity = 0;
+deskOpen.style.opacity = 0;
 desk.style.opacity = 0;
+explorePiano.style.opacity = 0;
 piano.style.opacity = 0;
 
 //This takes the optionTexts id and says for the game to start at 1
@@ -24,6 +38,37 @@ let currentId = 1;
 // We start the game here!
 function startGame() {
   updateText(currentId);
+}
+
+blueDoor.addEventListener("click", function () {
+  lockedDoorText.style.visibility = "visible";
+  lockedDoorText.style.opacity = 1;
+  lockedDoorText.style.transition = "opacity .5s";
+  hideLockedText();
+});
+
+redDoor.addEventListener("click", function () {
+  lockedDoorText.style.visibility = "visible";
+  lockedDoorText.style.opacity = 1;
+  lockedDoorText.style.transition = "opacity .5s";
+  hideLockedText();
+});
+
+greenDoor.addEventListener("click", function () {
+  lockedDoorText.style.visibility = "visible";
+  lockedDoorText.style.opacity = 1;
+  lockedDoorText.style.transition = "opacity .5s";
+  hideLockedText();
+});
+
+function hideLockedText() {
+  setTimeout(function () {
+    lockedDoorText.style.opacity = 0;
+    lockedDoorText.style.transition = "opacity 1.5s";
+  }, 500);
+  setTimeout(function () {
+    lockedDoorText.style.visibility = "hidden";
+  }, 1000);
 }
 
 brownDoor.addEventListener("click", function () {
@@ -42,6 +87,54 @@ brownDoor.addEventListener("click", function () {
   brownDoorBg.style.transition = "opacity .8s";
   displayObjects();
   updateText(2);
+});
+
+bookshelf.addEventListener("click", function () {
+  exploreBookshelf.style.visibility = "visible";
+  exploreBookshelf.style.opacity = 1;
+  exploreBookshelf.style.transition = "opacity 1.5s";
+  bookshelf.style.visibility = "hidden";
+  bookshelf.style.opacity = 0;
+  bookshelf.style.transition = "opacity 1.5s";
+  desk.style.visibility = "hidden";
+  desk.style.opacity = 0;
+  desk.style.transition = "opacity 1.5s";
+  piano.style.visibility = "hidden";
+  piano.style.opacity = 0;
+  piano.style.transition = "opacity 1.5s";
+  updateText(3);
+});
+
+desk.addEventListener("click", function () {
+  deskOpen.style.visibility = "visible";
+  deskOpen.style.opacity = "1";
+  deskOpen.style.transition = "opacity 1.5s";
+  bookshelf.style.visibility = "hidden";
+  bookshelf.style.opacity = 0;
+  bookshelf.style.transition = "opacity 1.5s";
+  desk.style.visibility = "hidden";
+  desk.style.opacity = 0;
+  desk.style.transition = "opacity 1.5s";
+  piano.style.visibility = "hidden";
+  piano.style.opacity = 1;
+  piano.style.transition = "opacity 1.5s";
+  updateText(4);
+});
+
+piano.addEventListener("click", function () {
+  explorePiano.style.visibility = "visible";
+  explorePiano.style.opacity = "1";
+  explorePiano.style.transition = "opacity 1.5s";
+  bookshelf.style.visibility = "hidden";
+  bookshelf.style.opacity = 0;
+  bookshelf.style.transition = "opacity 1.5s";
+  desk.style.visibility = "hidden";
+  desk.style.opacity = 0;
+  desk.style.transition = "opacity 1.5s";
+  piano.style.visibility = "hidden";
+  piano.style.opacity = 0;
+  piano.style.transition = "opacity 1.5s";
+  updateText(5);
 });
 
 //This is my fade in animation
